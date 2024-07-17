4857793b3d2a875b65ff15e3e3011f23f82e9b7d
montleee <144028935+montleee@users.noreply.github.com>
2024-07-17 11:29:23 +0300

    qusetion store

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, {useEffect, useState} from 'react'
-import useUserData from '@/lib/stores/userDataStore';
 import Container from "@/components/ui/container/Container";
 import Profile from "@/components/profile/Profile";
 
@@ -8,30 +7,7 @@ import Profile from "@/components/profile/Profile";
 
 const Page = () => {
 
-    // const { data, fetchUserDataByToken } = useUserData();
-    //
-    // const [token, setToken] = useState<string>("");
-    //
-    // useEffect(() => {
-    //     if (typeof window !== 'undefined') {
-    //         const tokenString = localStorage.getItem('token');
-    //         if (tokenString) {
-    //             try {
-    //                 const tokenObject = JSON.parse(tokenString);
-    //                 const token = tokenObject?.state?.token || '';
-    //                 setToken(token);
-    //                 if (token) {
-    //                     fetchUserDataByToken(token);
-    //                 }
-    //             } catch (error) {
-    //                 console.error('Ошибка при разборе токена из localStorage:', error);
-    //                 setToken('');
-    //             }
-    //         } else {
-    //             setToken('');
-    //         }
-    //     }
-    // }, [fetchUserDataByToken, setToken]);
+
 
     return (
         <Container>
