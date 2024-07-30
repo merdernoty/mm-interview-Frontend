60bb139b1769006a8854ccbe4b2f3abbd91816bc
montleee <144028935+montleee@users.noreply.github.com>
2024-07-30 15:19:50 +0300

    mainBanner

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -4,16 +4,14 @@ import ThemeList from '@/components/homePage/themeList'
 import Link from 'next/link'
 import PreFooter from '@/components/ui/footer/PreFooter'
 import Spline from '@splinetool/react-spline/next'
+import MainBanner from './mainBanner'
 const HomePage = () => {
 
 
     return (
         <>
-        <div className="h-96 w-full bg-violet-300">
-        <Spline
-                    scene="https://prod.spline.design/hu4zPKRYKZVlbKNB/scene.splinecode"
-                />
-                </div>
+        
+            <MainBanner/>
             <ThemeList/>
             <PreFooter></PreFooter>
         </>
---
+++ src/components/homePage/mainBanner.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Spline from '@splinetool/react-spline/next'
+const MainBanner = () => {
+
+
+    return (
+        <>
+        <div className="h-96 w-full bg-violet-300">
+        <Spline
+                    scene="https://prod.spline.design/hu4zPKRYKZVlbKNB/scene.splinecode"
+                />
+                </div>
+        
+        </>
+    )
+}
+
+export default MainBanner
