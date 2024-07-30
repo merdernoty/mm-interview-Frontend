822efbd36e3926ca301496e932e2f37ee9148e62
montleee <144028935+montleee@users.noreply.github.com>
2024-07-30 16:00:55 +0300

    added clsx

--- src/components/UI/footer/PreFooter.tsx
+++ src/components/UI/footer/PreFooter.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from 'react'
+import clsx from 'clsx'
 
 const PreFooter = () => {
+   
+    const dividerClass = clsx(
+        'absolute top-0 left-1/2 w-1/2 border-t border-transparent',
+        'border-gradient-gray shadow-[0_1px_10px_rgba(76,44,164,0.8)] z-0',
+    )
+
+    const containerClass = clsx(
+        'bg-[radial-gradient(145%_105%_at_45%_20%,_transparent_40%,_#63e_75%,_#c8c8ff_95%)]',
+        'h-[300px] flex items-center justify-center rounded-lg p-5 shadow-[0_20px_20px_rgba(76,44,164,0.2)]',
+    )
+
     return (
         <>
             <div className="relative">
-                <div className="absolute top-0 left-1/2 w-1/2 border-t border-transparent border-gradient-gray shadow-[0_1px_10px_rgba(76,44,164,0.8)] z-0" />
+                <div className={dividerClass} />
                 <div className="relative px-12 py-5">
-                    <div className="absolute top-0 left-1/2 w-1/2 border-t border-transparent border-gradient-gray shadow-[0_1px_10px_rgba(76,44,164,0.8)] z-0" />
-                    <div className="bg-[radial-gradient(145%_105%_at_45%_20%,_transparent_40%,_#63e_75%,_#c8c8ff_95%)] h-[400px] flex items-center justify-center rounded-lg p-5 shadow-[0_20px_20px_rgba(76,44,164,0.2)]">
-                        <div>
-                        </div>
+                    <div className={dividerClass} />
+                    <div className={containerClass}>
+                        <div></div>
                     </div>
                 </div>
             </div>
         </>
-    );
-};
+    )
+}
 
-export default PreFooter;
+export default PreFooter
