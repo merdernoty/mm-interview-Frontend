d9a647169517001f69faaa65de2fabd27b68d617
montleee <144028935+montleee@users.noreply.github.com>
2024-07-30 14:47:47 +0300

    refacor mainpage

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -5,15 +5,12 @@ import Link from 'next/link'
 import PreFooter from '@/components/ui/footer/PreFooter'
 import Spline from '@splinetool/react-spline/next'
 const HomePage = () => {
-    const { loading, error, data } = useGetThemesQuery()
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error: {error.message}</p>
 
     return (
         <>
-        <div className="h-96 w-full bg-violet-200">
-                <Spline
+        <div className="h-96 w-full bg-violet-300">
+        <Spline
                     scene="https://prod.spline.design/hu4zPKRYKZVlbKNB/scene.splinecode"
                 />
                 </div>
